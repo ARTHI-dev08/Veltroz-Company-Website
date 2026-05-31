360d17c78b3adab759444798ff0e30a52c381108
ARTHI-dev08 <arthip4505@gmail.com>
2026-05-31 22:03:24 +0530

    added application prop

--- backend/src/main/java/com/veltroz/chatbot/VeltrozChatbotApplication.java
+++ backend/src/main/java/com/veltroz/chatbot/VeltrozChatbotApplication.java
@@ -2,8 +2,10 @@ package com.veltroz.chatbot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.veltroz.chatbot"}) 
 public class VeltrozChatbotApplication {
     public static void main(String[] args) {
         SpringApplication.run(VeltrozChatbotApplication.class, args);
